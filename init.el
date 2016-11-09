fcf70c1295f590cebd1adec0c0e70c09dbbf0c72
califerno <califerno@gmail.com>
2016-11-09 17:31:09 +0100

    Modeline

--- init.el
+++ init.el
@@ -263,6 +263,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "white" :foreground "black" :box (:line-width 1 :color "black" :style released-button)))))
  '(region ((t (:background "#3D8CFF" :foreground "#ffffff")))))
 
 ;; Disable menu bar
