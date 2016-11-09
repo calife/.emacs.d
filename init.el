bb78a644e37b3769dba531c83a8d3ac11fd0c67f
califerno <califerno@gmail.com>
2016-11-09 17:26:06 +0100

    Changed selected region face

--- init.el
+++ init.el
@@ -263,7 +263,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(region ((t (:background "#3D8CFF" :foreground "#ffffff")))))
 
 ;; Disable menu bar
 (menu-bar-mode -1)
