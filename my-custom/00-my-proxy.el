393299f95958cc54f75b300c6fe8b7e356032e98
califerno <califerno@gmail.com>
2016-11-10 18:23:58 +0100

    save

--- my-custom/00-my-proxy.el
+++ my-custom/00-my-proxy.el
@@ -1,4 +1,3 @@
-
 (setq url-proxy-services
    '(("no_proxy" . "^\\(localhost\\|10.*\\)")
      ("http" . "192.168.214.121:8080")
@@ -7,4 +6,4 @@
 (setq url-http-proxy-basic-auth-storage
     (list (list "192.168.214.121:8080"
                 (cons "Input your PROXY UID !"
-                      (base64-encode-string "sky\\puccima:xxxxx#")))))
+                      (base64-encode-string "sky\\puccima:Settembre2016#")))))
