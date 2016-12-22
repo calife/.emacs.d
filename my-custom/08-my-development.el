f9fad490354fb25d65785ae78d4f4ed12938e234
califerno <califerno@gmail.com>
2016-12-22 22:26:33 +0100

    stable before update

--- my-custom/08-my-development.el
+++ my-custom/08-my-development.el
@@ -47,3 +47,9 @@
 (global-set-key (kbd "s-p") 'projectile-find-file)
 ;; Press Command-b for fuzzy switch buffer
 (global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
+
+;; ;; (add-to-list 'load-path "/path/to/dash-at-point")
+;; (autoload 'dash-at-point "dash-at-point"
+;;           "Search the word at point with Dash." t nil)
+;; (global-set-key "\C-cd" 'dash-at-point)
+;; (global-set-key "\C-ce" 'dash-at-point-with-docset)
