7d01b0ae13d0ddec5280cc5ced32b60e72d4c722
Marcello <marcello.pucci@meware.it>
2017-12-15 18:10:07 +0100

    Default font changed

--- my-custom/emacs-custom.el
+++ my-custom/emacs-custom.el
@@ -34,7 +34,6 @@
  '(linum-format " %4d    ")
  '(max-lisp-eval-depth 32000)
  '(max-specpdl-size 32000)
- '(menu-bar-mode t)
  '(mouse-wheel-mode t)
  '(nrepl-message-colors
    (quote
@@ -85,5 +84,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 90 :width normal))))
  '(linum ((t (:height 0.75))))
  '(tool-bar ((t (:background "black" :foreground "snow" :box (:line-width 1 :style released-button))))))
